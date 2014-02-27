ecc223cbc655d0687556a69ad86fe59ae5c53654
fengmk2 <fengmk2@gmail.com>
2014-02-27 12:37:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+koa-fresh
+=======
+
+[![Build Status](https://secure.travis-ci.org/fengmk2/koa-fresh.png)](http://travis-ci.org/fengmk2/koa-fresh) [![Coverage Status](https://coveralls.io/repos/fengmk2/koa-fresh/badge.png)](https://coveralls.io/r/fengmk2/koa-fresh) [![Dependency Status](https://gemnasium.com/fengmk2/koa-fresh.png)](https://gemnasium.com/fengmk2/koa-fresh)
+
+[![NPM](https://nodei.co/npm/koa-fresh.png?downloads=true&stars=true)](https://nodei.co/npm/koa-fresh/)
+
+![logo](https://raw.github.com/fengmk2/koa-fresh/master/logo.png)
+
+koa-fresh desc
+
+## Install
+
+```bash
+$ npm install koa-fresh
+```
+
+## Usage
+
+```js
+var koa-fresh = require('koa-fresh');
+
+koa-fresh.foo(function (err) {
+
+});
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2014 fengmk2 &lt;fengmk2@gmail.com&gt; and other contributors
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/fresh.test.js
@@ -0,0 +1,116 @@
+/**!
+ * koa-fresh - test/fresh.test.js
+ *
+ * Copyright(c) 2014
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var koa = require('koa');
+var should = require('should');
+var request = require('supertest');
+var etag = require('koa-etag');
+var fresh = require('../');
+
+describe('fresh.test.js', function () {
+  describe('when response status 50x', function () {
+    it('should pass through', function (done) {
+      var app = koa();
+      app.use(fresh());
+      app.use(etag())
+
+      app.use(function *(next) {
+        this.status = 500;
+        this.body = {error: 'err'};
+      });
+
+      request(app.listen())
+      .get('/')
+      .expect({error: 'err'})
+      .expect(500, done);
+    });
+  });
+
+  describe('when response status 20x', function () {
+    it('should request not etag return 200', function (done) {
+      var app = koa();
+      app.outputErrors = true;
+      app.use(fresh());
+      app.use(etag())
+
+      app.use(function *(next) {
+        this.status = 200;
+        this.body = {hi: 'foo'};
+      });
+
+      request(app.listen())
+      .get('/')
+      .expect('etag', '"-2137833482"')
+      .expect({hi: 'foo'})
+      .expect(200, done);
+    });
+
+    it('should request with etag return 304', function (done) {
+      var app = koa();
+      app.outputErrors = true;
+      app.use(fresh());
+      app.use(etag())
+
+      app.use(function *(next) {
+        this.status = 200;
+        this.body = {hi: 'foo'};
+      });
+
+      request(app.listen())
+      .get('/')
+      .set('If-None-Match', '"-2137833482"')
+      .expect('etag', '"-2137833482"')
+      .expect('')
+      .expect(304, done);
+    });
+
+    it('should clean body when status 204', function (done) {
+      var app = koa();
+      app.outputErrors = true;
+      app.use(fresh());
+      app.use(etag())
+
+      app.use(function *(next) {
+        this.status = 204;
+        this.body = {hi: 'foo'};
+      });
+
+      request(app.listen())
+      .get('/')
+      .expect('')
+      .expect(204, done);
+    });
+  });
+
+  describe('when response status 301 or 302', function () {
+    it('should request with etag return source status', function (done) {
+      var app = koa();
+      app.outputErrors = true;
+      app.use(fresh());
+      app.use(etag())
+
+      app.use(function *(next) {
+        this.redirect('/foo');
+      });
+
+      request(app.listen())
+      .get('/')
+      .set('If-None-Match', '"-2137833482"')
+      .expect('Redirecting to <a href="/foo">/foo</a>.')
+      .expect(302, done);
+    });
+  });
+});
